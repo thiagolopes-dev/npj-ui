4ab5ab90a3f28574b6641bb768b9c49a801459aa
Toxye1 <toxye2405@gmail.com>
2023-09-18 19:55:43 -0300

    clientes e varas

--- src/app/core/errorhandler.service.ts
+++ src/app/core/errorhandler.service.ts
@@ -1,3 +1,5 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { InstantiateExpr } from "@angular/compiler";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { MessageService } from "primeng/api";
@@ -11,5 +13,25 @@ export class ErrorHandleService {
         private router: Router
     ) { }
 
-    handle(errorResponse: any) { }
+    handle(errorResponse: any) { 
+        let msg: string;
+
+        if(typeof errorResponse === 'string'){
+            msg = errorResponse;
+        } else if(errorResponse instanceof NotAuthenticatedError){
+            msg = 'Sua conexão expirou!';
+            this.router.navigate(['/login']);
+        } else if(errorResponse instanceof HttpErrorResponse && errorResponse.status 
+            >= 400 && errorResponse.status <= 499){
+                let error;
+                if(errorResponse.error.message === undefined){
+                    msg = 'Ocorreu algum erro no aplicativo, tente novamente '
+                } else {
+                    msg =  errorResponse.error.message;
+                }
+                if(){
+
+                }
+        }
+    }
 }
