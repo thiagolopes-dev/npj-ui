6bebd09262640cb96273db5246c0e42ac5e506ff
Toxye1 <toxye2405@gmail.com>
2023-09-18 20:09:01 -0300

    errorhandler quase completo

--- src/app/core/errorhandler.service.ts
+++ src/app/core/errorhandler.service.ts
@@ -1,37 +1,81 @@
-import { HttpErrorResponse } from "@angular/common/http";
-import { InstantiateExpr } from "@angular/compiler";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { MessageService } from "primeng/api";
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Injectable()
-
 export class ErrorHandleService {
+  constructor(
+    private messageService: MessageService,
+    private router: Router,
+  ) {}
 
-    constructor(
-        private messageService: MessageService,
-        private router: Router
-    ) { }
+  handle(errorResponse: any) {
+    let msg: string;
 
-    handle(errorResponse: any) { 
-        let msg: string;
+    if (typeof errorResponse === 'string') {
+      msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua conexão expirou!';
+      this.router.navigate(['/login']);
+    } else if (
+      errorResponse instanceof HttpErrorResponse &&
+      errorResponse.status >= 400 &&
+      errorResponse.status <= 499
+    ) {
+      let errors;
+      if (errorResponse.error.message === undefined) {
+        msg = 'Ocorreu algum erro no aplicativo, tente novamente ';
+      } else {
+        msg = errorResponse.error.message;
+      }
+      if (errorResponse.status === 403) {
+        msg = 'Você não tem permissão executar essa ação';
+      }
 
-        if(typeof errorResponse === 'string'){
-            msg = errorResponse;
-        } else if(errorResponse instanceof NotAuthenticatedError){
-            msg = 'Sua conexão expirou!';
-            this.router.navigate(['/login']);
-        } else if(errorResponse instanceof HttpErrorResponse && errorResponse.status 
-            >= 400 && errorResponse.status <= 499){
-                let error;
-                if(errorResponse.error.message === undefined){
-                    msg = 'Ocorreu algum erro no aplicativo, tente novamente '
-                } else {
-                    msg =  errorResponse.error.message;
-                }
-                if(errorResponse.status === 403){
-                    msg = 'Você não tem permissão executar essa ação';
-              }
-        }
+      try {
+        errors = errorResponse.error;
+        msg = errors[0].mensagemUsurario;
+      } catch (e) {}
+    } else if (
+      errorResponse instanceof HttpErrorResponse &&
+      errorResponse.status >= 500 &&
+      errorResponse.status <= 599
+    ) {
+      let errors;
+      if (errorResponse.error.message === undefined) {
+        msg = 'Ocorreu um erro no servidor...';
+      } else {
+        msg = errorResponse.error.message;
+      }
+      try {
+        errors = errorResponse.error;
+        msg = errors[0].mensagemUsurario;
+      } catch (e) {}
+    } else if (
+      errorResponse instanceof HttpErrorResponse &&
+      errorResponse.status >= 300 &&
+      errorResponse.status <= 399
+    ) {
+      let errors;
+      if (errorResponse.error.message === undefined) {
+        msg = 'Ocorreu um erro de redirecionamento...';
+      } else {
+        msg = errorResponse.error.message;
+      }
+      try {
+        errors = errorResponse.error;
+        msg = errors[0].mensagemUsurario;
+      } catch (e) {}
     }
+
+    if (msg == undefined || msg === null) {
+      this.messageService.add({
+        severity: 'erro',
+        detail: ' ${errorResponse}',
+      });
+    } else {
+      this.messageService.add({ severity: 'error', detail: '${msg}' });
+    }
+  }
 }
