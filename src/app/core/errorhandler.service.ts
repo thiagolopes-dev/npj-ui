11a889fa11f5ef5c694ffb57b26892bb6d4c271c
Thiago Lopes <thiagoolopezz@gmail.com>
2023-11-13 18:09:29 -0300

    Correção ErrorHandler e Motivo Cadastro

--- src/app/core/errorhandler.service.ts
+++ src/app/core/errorhandler.service.ts
@@ -1,15 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-// import { NotAuthenticatedError } from '../demo/components/pages/seguranca/login/daring-http';
 
 @Injectable()
 export class ErrorHandlerService {
-
   constructor(
     private messageService: MessageService,
-    private router: Router
   ) { }
 
   handle(errorResponse: any) {
@@ -17,69 +13,54 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof HttpErrorResponse) {
+      if (errorResponse.status >= 400 && errorResponse.status <= 499) {
+        let errorMessage = 'Ocorreu um erro no app, tente novamente...';
 
-    } else if (errorResponse instanceof HttpErrorResponse
-      && errorResponse.status >= 400 && errorResponse.status <= 499) {
-      let errors;
-      if (errorResponse.error.mensagem === undefined) {
-        msg = 'Ocorreu algum erro no app, tente novamente...';
-      } else {
-        msg = errorResponse.error.mensagem;
-      }
-
-      if (errorResponse.status === 403) {
-        if (errorResponse.error.mensagem === undefined) {
-          msg = 'Você não tem permissão para executar esta ação';
-        } else {
-          msg = errorResponse.error.mensagem;
-          // console.log(msg);
+        if (errorResponse.error && errorResponse.error.message) {
+          if (Array.isArray(errorResponse.error.message)) {
+            errorMessage = errorResponse.error.message[0];
+          } else {
+            errorMessage = errorResponse.error.message;
+          }
         }
+
+        if (errorResponse.status === 403) {
+          errorMessage = 'Você não tem permissão para executar esta ação';
+        }
+
+        msg = errorMessage;
+      } else if (errorResponse.status >= 500 && errorResponse.status <= 599) {
+        let errorMessage = 'Ocorreu um erro no servidor...';
+
+        if (errorResponse.error && errorResponse.error.message) {
+          if (Array.isArray(errorResponse.error.message)) {
+            errorMessage = errorResponse.error.message[0];
+          } else {
+            errorMessage = errorResponse.error.message;
+          }
+        }
+
+        msg = errorMessage;
+      } else if (errorResponse.status >= 300 && errorResponse.status <= 399) {
+        let errorMessage = 'Ocorreu um erro de redirecionamento...';
+
+        if (errorResponse.error && errorResponse.error.message) {
+          if (Array.isArray(errorResponse.error.message)) {
+            errorMessage = errorResponse.error.message[0];
+          } else {
+            errorMessage = errorResponse.error.message;
+          }
+        }
+
+        msg = errorMessage;
       }
-
-      try {
-        errors = errorResponse.error;
-
-        msg = errors[0].mensagemUsuario;
-      } catch (e) { }
-
-      //    console.error('Ocorreu um erro', errorResponse);
-
-    }
-    else if (errorResponse instanceof HttpErrorResponse
-      && errorResponse.status >= 500 && errorResponse.status <= 599) {
-      let errors;
-      if (errorResponse.error.mensagem === undefined) {
-        msg = 'Ocorreu um erro no servidor...';
-      } else {
-        msg = errorResponse.error.mensagem;
-      }
-      try {
-        errors = errorResponse.error;
-
-        msg = errors[0].mensagemUsuario;
-      } catch (e) { }
-    }
-    else if (errorResponse instanceof HttpErrorResponse
-      && errorResponse.status >= 300 && errorResponse.status <= 399) {
-      let errors;
-      if (errorResponse.error.mensagem === undefined) {
-        msg = 'Ocorreu um erro de redirecionamento...';
-      } else {
-        msg = errorResponse.error.mensagem;
-      }
-      try {
-        errors = errorResponse.error;
-
-        msg = errors[0].mensagemUsuario;
-      } catch (e) { }
     }
 
     if (msg === undefined || msg === null) {
       this.messageService.add({ severity: 'error', detail: `${errorResponse}` });
-    }
-    else {
+    } else {
       this.messageService.add({ severity: 'error', detail: `${msg}` });
     }
   }
-
 }
